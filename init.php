e7dbe851877a846d9b5758c5d2594eb30796909b
github-lai <373515719@qq.com>
2020-04-27 05:08:07 +0800

    error记录机制和大小写敏感问题处理

--- init.php
+++ init.php
@@ -6,16 +6,37 @@ ini_set("short_open_tag","On");
 
 define("ROOTDIR",getcwd().'/');
 
-Lib\Config::init(ROOTDIR."usr/config.ini");//初始化用户配置文件
+Lib\Config::init(ROOTDIR."usr/config.ini");
 if(Lib\Config::has('auth')){
-	die('Loader::init alert : config.ini items are not allow named like "auth", change it to another');
+	die('init alert : config.ini items are not allow named like "auth", change it to another');
 }
-Lib\Config::init(ROOTDIR."usr/auth.ini");//初始化认证器配置文件
+Lib\Config::init(ROOTDIR."usr/auth.ini");
 
-if(Lib\Config::$env != "live"){
+if(Lib\Config::get('debug') == "true"){
 	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 }else{
 	error_reporting(0);//display nothing online
 }
+
+function trigger()
+{
+	$_error = error_get_last();
+	if ($_error && in_array($_error['type'], array(1, 4, 16, 64, 256, 4096, E_ALL))) 
+	{
+		if(Lib\Config::get('debug') == "true"){
+			//output directly
+			echo '<font color=red>Error 500</font></br>';
+			echo 'message : ' . $_error['message'] . '</br>';
+			echo 'file : ' . $_error['file'] . '</br>';
+			echo 'line : ' . $_error['line'] . '</br>';
+		}else{
+			//log
+			$msg = var_export($_error,true);
+			Lib\Helper::error($msg);
+		}
+	}
+}
+
+register_shutdown_function('trigger');
 
 ?>
